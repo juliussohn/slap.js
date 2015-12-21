475de60231056c3b8f2d82eac1117e835af6e666
Julius Sohn <juliussohn@leicht-luftig.de>
2015-12-21 11:22:48 +0100

    jo

--- src/jquery.slap.js
+++ src/jquery.slap.js
@@ -8,7 +8,6 @@
             pathToSlap: "slap.js/dist/",
             slapDuration: 200,
             slapIntensity: 1,
-            slapEasing: 'swing'
         }, options);
         swooshAudio.setAttribute('src', settings.pathToSlap + 'swoosh.mp3');
         slapAudio.setAttribute('src', settings.pathToSlap + 'slap.mp3');
@@ -16,6 +15,7 @@
             var self = $(this);
             self.css({
                 'cursor': 'pointer',
+                'transition': 'transform '+settings.slapDuration+'ms ',
                 borderSpacing: 0,
             });
             if (self.css("display") != "block" && self.css("display") != "inline-block") {
@@ -24,6 +24,9 @@
             self.on("mousedown", function () {
                 swooshAudio.play();
             });
+            var x=0,
+                y=0,
+                prevdeg=0;
             self.on("mouseup", function (event) {
                 slapAudio.play();
                 halfX = (self.width() / 2);
@@ -32,8 +35,7 @@
                 coordX = event.offsetX - halfX;
                 coordY = event.offsetY - halfY;
                 radius = Math.sqrt((coordX * coordX) + (coordY * coordY))
-                intensity = radius / longerSide;
-                console.log(intensity)
+                intensity = (radius / longerSide) ;
                 if (coordX > 0) {
                     // console.log( "X", ((Math.atan2(coordX, coordY) / Math.PI) * 2) - 1);
                     x = ((Math.atan2(coordX, coordY) / Math.PI) * 2) - 1;
@@ -50,25 +52,18 @@
                     console.log("Y", ((Math.atan2(coordX, coordY) / Math.PI) * 2));
                     y = ((Math.atan2(coordX, coordY) / Math.PI) * 2);
                 }
-                console.log(Math.round(x * 10) / 10, Math.round(y * 10) / 10);
-                // console.log( "Y", ((Math.atan2(coordX, coordY) / Math.PI) * 2 ) + 2);
-                //var x =  ((Math.atan2(coordX, coordY) / Math.PI) * 2) -1
-                //var y =  ((Math.atan2(coordX, coordY) / Math.PI) * -2 ) + 2
-                //event.offsetX
-                //event.offsetY
-                //self.width();
-                //self.height();
+              
+            
                 var stepCount = 0;
-                //oben:      -1     0     0   ATAN 1
-                //o r      -0.5   0.5     0   ATAN 0.75
-                //rechts:     0     1     0   ATAN 0.5
-                //ur    :   0.5   0.5     0   ATAN 0.25
-                //unten:      1     0     0   ATAN 0
-                //ul   :    0.5  -0.5     0   ATAN -0.25
-                //links:      0    -1     0   ATAN -0.5
-                //ol:      -0.5  -0.5     0   ATAN -0.75
-                //var x = 0.5//Math.random() * 2 - 1;
-                //var y = 0.5//Math.random() * 2 - 1;
+
+
+                console.log(360 * intensity * settings.slapIntensity)
+                self
+                            .css({
+                                'transform': 'rotate3d(' + (x) + ',' + (y) + ', 0 ,' + (360 * intensity * settings.slapIntensity) + 'deg)'
+                            });
+                /*
+                console.log((  self.css("transform")));
                 var z = 0; //Math.random() * 2 - 1;
                 self.animate({
                     borderSpacing: -90
@@ -82,14 +77,14 @@
 
                        
                         deg = -c * ((t=t/d-1)*t*t*t - 1) + b;
-                        
+                        prevdeg = deg;
                         $(this)
                             .css({
                                 'transform': 'rotate3d(' + (x) + ',' + (y) + ',' + (z) + ',' + (deg) + 'deg)'
                             });
                     },
                     duration: settings.slapDuration
-                }, "easeOutQuad");
+                }, "easeOutQuad",function(){alert("finished")});*/
             });
         });
     };
